338739e3aa280b4714b2ecb59f109d42929217c4
Sebastian <sebjerume@gmail.com>
2025-06-06 09:10:02 -0400

    more to schema

--- schema.sql
+++ schema.sql
@@ -4,21 +4,36 @@ create database if not exists dealership_db;
 
 use dealership_db;
 
-create table dealerships( 
-dealership_id integer not null auto_increment,
- name VARCHAR(50) not null, 
-address VARCHAR(50),
- phone VARCHAR(12), 
- primary key ('dealership_id') 
- );
-
-create table vehicles (
-vin varchar(17),
-make varchar(50),
-model varchar(50),
-year INT,
-color varchar(20),
-price decimal(10,2),
-sold boolean,
-primary key('VIN')
+CREATE TABLE dealerships (
+    dealership_id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    address VARCHAR(50),
+    phone VARCHAR(12),
+     PRIMARY KEY (`dealership_id`)
 );
+
+CREATE TABLE vehicles (
+	VIN VARCHAR(17),
+    make VARCHAR(50),
+    model VARCHAR(50),
+    year INT,
+    color VARCHAR(20),
+    price DECIMAL(10,2),
+    sold BOOLEAN, 
+    PRIMARY KEY(`VIN`)
+    );
+    
+    CREATE TABLE inventory (
+    dealership_id INT,
+    VIN VARCHAR(17),
+    PRIMARY KEY(dealership_id, VIN)
+    );
+    
+    CREATE TABLE sales_contracts (
+    contract_id INT auto_increment,
+    VIN VARCHAR(17),
+    sale_date Date,
+    sale_price DECIMAL(10,2),
+    customer_name VARCHAR(50),
+    PRIMARY KEY (`contract_id`)
+    );
